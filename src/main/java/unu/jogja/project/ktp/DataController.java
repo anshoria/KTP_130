3d63cb24b87b1bee1988956e856549935cb8c6f2
anshoria <anshoriakbar11@gmail.com>
2022-05-21 01:35:19 +0800

    Commit 2

--- src/main/java/unu/jogja/project/ktp/DataController.java
+++ src/main/java/unu/jogja/project/ktp/DataController.java
@@ -4,8 +4,12 @@
  */
 package unu.jogja.project.ktp;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -13,8 +17,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class DataController {
+    
+    DataJpaController datactrl = new DataJpaController();
+    List<Data> newdata = new ArrayList<>();
+    
+    @RequestMapping("/main")
+    public String getMain(){
+        return "menu";
+    }
+    
     @RequestMapping("/data")
-    public String getDataKTP(){
+    //@ResponseBody
+    public String getDataKTP(Model model){
+        int record = datactrl.getDataCount();
+        String result="";
+        try {
+            newdata = datactrl.findDataEntities().subList(0, record);
+        } catch (Exception e) {
+            result = e.getMessage();
+        }
+        
+        model.addAttribute("goData", newdata);
+        model.addAttribute("record", record);
+                
         return "database";
     }
+    
+    @RequestMapping("/edit")
+    public String doEdit(){
+        return "editktp";
+    }
+    
 }
---
+++ src/main/java/unu/jogja/project/ktp/coba/DummyController.java
@@ -0,0 +1,82 @@
+package unu.jogja.project.ktp.coba;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author msi gf75
+ */
+@Controller
+public class DummyController {
+
+    DummyJpaController dummyController = new DummyJpaController();
+    List<Dummy> data = new ArrayList<>();
+
+    @RequestMapping("/dummy")
+    //@ResponseBody
+    public String getDummy (Model model) {
+    int record = dummyController.getDummyCount();
+        String result = "";
+        
+        try{
+            data = dummyController.findDummyEntities().subList(0, record);
+        }
+        catch (Exception e){
+            result=e.getMessage();
+        }
+        
+        model.addAttribute("godummy", data);
+        model.addAttribute("record", record);
+         
+        return "dummy";    
+    }
+    
+    @RequestMapping("/create")
+    public String createDummy() {
+
+        return "dummy/create";
+    }
+
+    @PostMapping(value = "/newdata", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String newDummy(HttpServletRequest data, @RequestParam("gambar") MultipartFile file) throws ParseException, Exception {
+
+        Dummy dumdata = new Dummy();
+
+        String id = data.getParameter("id");
+        int iid = Integer.parseInt(id);
+
+        String tanggal = data.getParameter("tanggal");
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(tanggal);
+
+        //String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        byte[] image = file.getBytes();
+
+        dumdata.setId(iid);
+        dumdata.setTanggal(date);
+        dumdata.setGambar(image);
+
+        return "dummy/create";
+    }
+    
+    @RequestMapping (value="/image" , method = RequestMethod.GET ,produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
+    public ResponseEntity<byte[]> getImg(@RequestParam("id") int id) throws Exception {
+	Dummy dumdata = dummyController.findDummy(id);
+	byte[] image = dumdata.getGambar();
+	return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(image);
+    }
+}
